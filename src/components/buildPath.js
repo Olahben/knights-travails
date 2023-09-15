6d8bee2999d65ad53a88f9f59370b47eb8a37d6d
Olahben <ohagenbendixen@gmail.com>
2023-09-15 11:42:51 +0100

    Create all the possible moves in a tree for knight Next step is to do it recursively

--- src/components/buildPath.js
+++ src/components/buildPath.js
@@ -2,21 +2,30 @@ import knight from "./knight.js";
 import board from "./board.js";
 
 function buildPath(position) {
-  const knightPosition = knight([3, 3]).position;
+  const knightPosition = knight([0, 0]).position;
   const boardArr = board();
   // console.log(knightPosition, boardArr);
 
   function buildTree(arr) {
     const root = knightPosition;
-    for (let i = 0; i < 9; i += 1) {
-      console.log(root);
-      if (root[0] - 2 > 0 && root[1] - 1 > 0) {
-        root.first = root.forEach((ele, index) => {
-          // console.log(ele, index);
-        });
-      }
-    }
+    root.first =
+      root[0] - 2 >= 0 && root[1] - 1 >= 0 ? [root[0] - 2, root[1] - 1] : false;
+    root.second =
+      root[0] - 2 >= 0 && root[1] + 1 < 8 ? [root[0] - 2, root[1] + 1] : false;
+    root.third =
+      root[0] - 1 >= 0 && root[1] - 2 >= 0 ? [root[0] - 1, root[1] - 2] : false;
+    root.fourth =
+      root[0] - 1 >= 0 && root[1] + 2 < 8 ? [root[0] - 1, root[1] + 2] : false;
+    root.fifth =
+      root[0] + 1 < 8 && root[1] - 2 >= 0 ? [root[0] + 1, root[1] - 2] : false;
+    root.sixth =
+      root[0] + 1 < 8 && root[1] + 2 < 8 ? [root[0] + 1, root[1] + 2] : false;
+    root.seventh =
+      root[0] + 2 < 8 && root[1] - 1 >= 0 ? [root[0] + 2, root[1] - 1] : false;
+    root.eighth =
+      root[0] + 2 < 8 && root[1] + 1 < 8 ? [root[0] + 2, root[1] + 1] : false;
+    console.log(root);
   }
   buildTree(boardArr);
 }
-buildPath([3, 3]);
+buildPath([0, 0]);
