adc291b3b239e551380e42aa2755d8eedf2bb930
Olahben <ohagenbendixen@gmail.com>
2023-09-17 08:36:21 +0100

    Fix data structure to be a tree

--- src/components/buildPath.js
+++ src/components/buildPath.js
@@ -6,32 +6,48 @@ function buildPath(position, desiredPosition) {
   function buildTree(root) {
     console.log("root", root, "desired position:", desiredPosition);
     // if (JSON.stringify(root) === JSON.stringify(desiredPosition)) return root;
-    const newRoot = {
+    /* const newRoot = {
       move: root.move,
       possibilities: {},
-    };
-    // console.log(newRoot);
+    }; */
+    // console.log(root);
     // Assigns all possible moves
-    newRoot.possibilities.first =
-      root.move[0] - 2 >= 0 && root.move[1] - 1 >= 0 ? [root.move[0] - 2, root.move[1] - 1] : false;
-    newRoot.possibilities.second =
-      root.move[0] - 2 >= 0 && root.move[1] + 1 < 8 ? [root.move[0] - 2, root.move[1] + 1] : false;
-    newRoot.possibilities.third =
-      root.move[0] - 1 >= 0 && root.move[1] - 2 >= 0 ? [root.move[0] - 1, root.move[1] - 2] : false;
-    newRoot.possibilities.fourth =
-      root.move[0] - 1 >= 0 && root.move[1] + 2 < 8 ? [root.move[0] - 1, root.move[1] + 2] : false;
-    newRoot.possibilities.fifth =
-      root.move[0] + 1 < 8 && root.move[1] - 2 >= 0 ? [root.move[0] + 1, root.move[1] - 2] : false;
-    newRoot.possibilities.sixth =
-      root.move[0] + 1 < 8 && root.move[1] + 2 < 8 ? [root.move[0] + 1, root.move[1] + 2] : false;
-    newRoot.possibilities.seventh =
-      root.move[0] + 2 < 8 && root.move[1] - 1 >= 0 ? [root.move[0] + 2, root.move[1] - 1] : false;
-    newRoot.possibilities.eighth =
-      root.move[0] + 2 < 8 && root.move[1] + 1 < 8 ? [root.move[0] + 2, root.move[1] + 1] : false;
-    return newRoot;
+    root.possibilities.first =
+      root.first[0] - 2 >= 0 && root.first[1] - 1 >= 0
+        ? [root.first[0] - 2, root.first[1] - 1]
+        : false;
+    root.possibilities.second =
+      root.first[0] - 2 >= 0 && root.first[1] + 1 < 8
+        ? [root.first[0] - 2, root.first[1] + 1]
+        : false;
+    root.possibilities.third =
+      root.first[0] - 1 >= 0 && root.first[1] - 2 >= 0
+        ? [root.first[0] - 1, root.first[1] - 2]
+        : false;
+    root.possibilities.fourth =
+      root.first[0] - 1 >= 0 && root.first[1] + 2 < 8
+        ? [root.first[0] - 1, root.first[1] + 2]
+        : false;
+    root.possibilities.fifth =
+      root.first[0] + 1 < 8 && root.first[1] - 2 >= 0
+        ? [root.first[0] + 1, root.first[1] - 2]
+        : false;
+    root.possibilities.sixth =
+      root.first[0] + 1 < 8 && root.first[1] + 2 < 8
+        ? [root.first[0] + 1, root.first[1] + 2]
+        : false;
+    root.possibilities.seventh =
+      root.first[0] + 2 < 8 && root.first[1] - 1 >= 0
+        ? [root.first[0] + 2, root.first[1] - 1]
+        : false;
+    root.possibilities.eighth =
+      root.first[0] + 2 < 8 && root.first[1] + 1 < 8
+        ? [root.first[0] + 2, root.first[1] + 1]
+        : false;
+    return root;
   }
   const result = buildTree({
-    move: knightPosition,
+    first: knightPosition,
     possibilities: {},
   });
   console.log(result);
