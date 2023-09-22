e69df2647fb728eacc7c10e5256b014c3b89532b
Olahben <ohagenbendixen@gmail.com>
2023-09-22 07:41:40 +0100

    Fix base case

--- src/components/buildPath.js
+++ src/components/buildPath.js
@@ -3,11 +3,9 @@ import knight from "./knight.js";
 function buildPath(position, desiredPosition) {
   const knightPosition = knight(position).position;
 
-  function buildTree(root, initPosition) {
-    console.log(root, initPosition);
-    root.first = initPosition;
-    root.possibilities = {};
-    if (root.first === desiredPosition) return;
+  function buildTree(root) {
+    console.log(root.first, desiredPosition);
+    if (JSON.stringify(root.first) === JSON.stringify(desiredPosition)) return;
     // Assigns all possible moves
     root.possibilities.first =
       root.first[0] - 2 >= 0 && root.first[1] - 1 >= 0
@@ -42,7 +40,6 @@ function buildPath(position, desiredPosition) {
         ? [root.first[0] + 2, root.first[1] + 1]
         : false;
 
-    console.log(root);
     for (let i = 0; i < Object.keys(root.possibilities).length; i += 1) {
       const keys = Object.keys(root.possibilities);
       const key = keys[i];
@@ -52,11 +49,15 @@ function buildPath(position, desiredPosition) {
       };
       console.log(newRoot);
       if (root.possibilities[key]) {
-        // buildTree(root.possibilities[key], root.possibilities[key]);
+        // buildTree(newRoot);
       }
     }
+    console.log(root);
   }
-  const result = buildTree({}, knightPosition);
+  const result = buildTree({
+    first: knightPosition,
+    possibilities: {},
+  });
   console.log(result);
 }
 buildPath([4, 4], [3, 3]);
