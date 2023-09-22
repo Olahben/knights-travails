d52f1e148105dee6c74430ae22b8d63818cff376
Olahben <ohagenbendixen@gmail.com>
2023-09-22 13:07:04 +0100

    Fix recursive calling

--- src/components/buildPath.js
+++ src/components/buildPath.js
@@ -31,39 +31,49 @@ function buildPath(position, desiredPosition) {
     if (moves.has(JSON.stringify(root.move))) return root;
     moves.add(JSON.stringify(root.move));
     // Assigns all possible moves
-    // Assigns all possible moves
-    root.possibilities.first.move =
+    console.log("Root before assigning:", root);
+    root.possibilities.first = createDataStructure(
       root.move[0] - 2 >= 0 && root.move[1] - 1 >= 0
         ? [root.move[0] - 2, root.move[1] - 1]
-        : false;
-    root.possibilities.second.move =
+        : false
+    );
+    root.possibilities.second = createDataStructure(
       root.move[0] - 2 >= 0 && root.move[1] + 1 < 8
         ? [root.move[0] - 2, root.move[1] + 1]
-        : false;
-    root.possibilities.third.move =
+        : false
+    );
+    root.possibilities.third = createDataStructure(
       root.move[0] - 1 >= 0 && root.move[1] - 2 >= 0
         ? [root.move[0] - 1, root.move[1] - 2]
-        : false;
-    root.possibilities.fourth.move =
+        : false
+    );
+    root.possibilities.fourth = createDataStructure(
       root.move[0] - 1 >= 0 && root.move[1] + 2 < 8
         ? [root.move[0] - 1, root.move[1] + 2]
-        : false;
-    root.possibilities.fifth.move =
+        : false
+    );
+    root.possibilities.fifth = createDataStructure(
       root.move[0] + 1 < 8 && root.move[1] - 2 >= 0
         ? [root.move[0] + 1, root.move[1] - 2]
-        : false;
-    root.possibilities.sixth.move =
+        : false
+    );
+    root.possibilities.sixth = createDataStructure(
       root.move[0] + 1 < 8 && root.move[1] + 2 < 8
         ? [root.move[0] + 1, root.move[1] + 2]
-        : false;
-    root.possibilities.seventh.move =
+        : false
+    );
+    root.possibilities.seventh = createDataStructure(
       root.move[0] + 2 < 8 && root.move[1] - 1 >= 0
         ? [root.move[0] + 2, root.move[1] - 1]
-        : false;
-    root.possibilities.eighth.move =
+        : false
+    );
+    root.possibilities.eighth = createDataStructure(
       root.move[0] + 2 < 8 && root.move[1] + 1 < 8
         ? [root.move[0] + 2, root.move[1] + 1]
-        : false;
+        : false
+    );
+
+    console.log("Root after assigning:", root);
 
     /* root.possibilities.first =
       root.first[0] - 2 >= 0 && root.first[1] - 1 >= 0
@@ -101,8 +111,9 @@ function buildPath(position, desiredPosition) {
     for (let i = 0; i < Object.keys(root.possibilities).length; i += 1) {
       const keys = Object.keys(root.possibilities);
       const key = keys[i];
-      if (root.possibilities[key]) {
-        buildTree(createDataStructure(root.possibilities[key]));
+      console.log("calling:", root.possibilities[key].move);
+      if (root.possibilities[key].move) {
+        buildTree(createDataStructure(root.possibilities[key].move));
       }
     }
     return root;
