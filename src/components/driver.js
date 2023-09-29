ccfb094511cb954befffb22723a95f2ce658155f
Olahben <ohagenbendixen@gmail.com>
2023-09-29 13:59:22 +0100

    Import parent array

--- src/components/driver.js
+++ src/components/driver.js
@@ -2,8 +2,11 @@ import searchForPath from "./search.js";
 import buildTree from "./buildPath.js";
 import board from "./board.js";
 import knight from "./knight.js";
+import { getParents } from "./buildPath.js";
 
 const knightPosition = knight([6, 4]).position;
 const tree = buildTree([5, 1], knightPosition);
 console.log(tree);
-searchForPath(tree, knightPosition, []);
+const parentsArr = getParents();
+console.log(parentsArr);
+// searchForPath(tree, knightPosition, []);
