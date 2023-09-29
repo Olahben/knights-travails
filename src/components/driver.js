e59fc41208960473571a9f47898b61616552545d
Olahben <ohagenbendixen@gmail.com>
2023-09-29 09:32:40 +0100

    Call searchForPath with an array

--- src/components/driver.js
+++ src/components/driver.js
@@ -6,4 +6,4 @@ import knight from "./knight.js";
 const knightPosition = knight([6, 4]).position;
 const tree = buildTree([5, 1], knightPosition);
 console.log(tree);
-searchForPath(tree, knightPosition);
+searchForPath(tree, knightPosition, []);
