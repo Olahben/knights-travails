376b596ec8cd1f09d221c51e061d3fdb5f9e4c76
Olahben <ohagenbendixen@gmail.com>
2023-09-29 09:26:33 +0100

    Import knightPosition and call searchForPath in tree

--- src/components/driver.js
+++ src/components/driver.js
@@ -1,3 +1,9 @@
 import searchForPath from "./search.js";
 import buildTree from "./buildPath.js";
 import board from "./board.js";
+import knight from "./knight.js";
+
+const knightPosition = knight([6, 4]).position;
+const tree = buildTree([5, 1], knightPosition);
+console.log(tree);
+searchForPath(tree, knightPosition);
